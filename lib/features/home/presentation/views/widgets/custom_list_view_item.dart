f38af11f5017a9372bca80365c19222344510890
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-02-14 02:15:22 -0800

    add custom list view item to home

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,29 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 170.w,
+      height: 210.h,
+      child: AspectRatio(
+        aspectRatio: 2 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                AssetsData.testImage,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
