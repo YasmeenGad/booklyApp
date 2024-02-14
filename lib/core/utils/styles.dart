c98593f6b51e33c9cceacb10c62a7e46dd6c1a61
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-02-14 09:19:09 -0800

    add text styles to app part 2

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,7 +1,20 @@
+import 'package:bookly/core/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class Styles {
   static TextStyle txtstyle22 =
       TextStyle(fontSize: 22.sp, fontWeight: FontWeight.w600);
+  static TextStyle txtstyle26 = TextStyle(
+      fontSize: 26.sp,
+      fontWeight: FontWeight.normal,
+      fontFamily: kGtSectraFine);
+  static TextStyle txtstyle18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle txtstyle20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w500,
+  );
 }
