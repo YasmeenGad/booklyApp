10cc92989e25ee46298e69d3a33760768150ba71
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-02-14 01:18:26 -0800

    init app and create splash view
    Signed-off-by: Yasmeen Gad <gadyasmeen335@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:bookly/features/Splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../../core/constants.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+          ),
+          initialRoute: SplashView.routeName,
+          getPages: [
+            GetPage(name: SplashView.routeName, page: () => const SplashView()),
+          ],
+        );
+      },
+    );
+  }
+}
